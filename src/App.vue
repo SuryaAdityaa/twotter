be600e93e85cc8e1e639b298d151591435eb3685
Surya Aditya <suryaaditya4177@gmail.com>
2021-03-15 15:03:16 +0800

    Router

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
-  <div id="app">
-    <UserProfile/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-
-import UserProfile from "./components/UserProfile";
-
-export default {
-  name: 'App',
-  components: { UserProfile },
-}
-</script>
-
-<style>
-* {
-  margin: 0;
-}
-
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
-  width: 100%;
-  background-color: #f1f1f1;
-  height: 100vh;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
